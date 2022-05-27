aa219d773137ac5afe75b3cbb6f57b3b2cd1510c
KaviiSuri <surikavii@gmail.com>
2022-05-28 00:52:24 +0530

    improved and customized data

--- executable_dotfiles.sh
+++ executable_dotfiles.sh
@@ -60,116 +60,10 @@ setup_dependencies() {
 
 setup_prompts() {
     printf -- "\n%sSetting up shell frameworks:%s\n\n" "$BOLD" "$RESET"
-
-    # Install Bash-it
-    PACKAGE_NAME='Bash-it'
-    printf -- "%sInstalling/updating %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/Bash-it/bash-it/archive/master.tar.gz' "${HOME}/.bash-it" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    # Install Oh My Zsh
-    PACKAGE_NAME='Oh My Zsh'
-    printf -- "%sInstalling/updating %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    CHEZMOIPATH=$(chezmoi source-path)
-    rm -rf "$CHEZMOIPATH"/dot_oh-my-zsh/plugins
-    import_repo 'https://github.com/robbyrussell/oh-my-zsh/archive/master.tar.gz' "${HOME}/.oh-my-zsh" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    # Install Zsh plugins
-    PACKAGE_NAME='zsh-autosuggestions'
-    printf -- "%sInstalling/updating Zsh plugin: %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/zsh-users/zsh-autosuggestions/archive/master.tar.gz' "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    PACKAGE_NAME='zsh-syntax-highlighting'
-    printf -- "%sInstalling/updating Zsh plugin: %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/zsh-users/zsh-syntax-highlighting/archive/master.tar.gz' "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    # Install Zsh themes
-    PACKAGE_NAME='Powerlevel9k'
-    printf -- "%sInstalling/updating Zsh theme: %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/Powerlevel9k/powerlevel9k/archive/master.tar.gz' "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel9k" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    PACKAGE_NAME='Powerlevel10k'
-    printf -- "%sInstalling/updating Zsh theme: %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/romkatv/powerlevel10k/archive/master.tar.gz' "${ZSH_CUSTOM:-${HOME}/.oh-my-zsh/custom}/themes/powerlevel10k" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
 }
 
 setup_applications() {
     printf -- "\n%sSetting up CLI applications:%s\n\n" "$BOLD" "$RESET"
-
-    # Install Oh My Tmux
-    PACKAGE_NAME='Oh My Tmux'
-    printf -- "%sInstalling/updating %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/gpakosz/.tmux/archive/master.tar.gz' "${HOME}/.tmux" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-    ln -s -f -v .tmux/.tmux.conf "$HOME"
-
-    # Install Tmux Plugin Manager
-    PACKAGE_NAME='Tmux Plugin Manager'
-    printf -- "%sInstalling/updating %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    CHEZMOIPATH=$(chezmoi source-path)
-    mkdir -pv "$CHEZMOIPATH"/dot_tmux/plugins/tpm
-    import_repo 'https://github.com/tmux-plugins/tpm/archive/master.tar.gz' "${HOME}/.tmux/plugins/tpm" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    # Install Nano syntax highlighting files
-    PACKAGE_NAME='Nano syntax highlighting'
-    CHEZMOIPATH=$(chezmoi source-path)
-    rm -rf "$CHEZMOIPATH"/dot_nano/nanorc
-    printf -- "%sInstalling/updating %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/scopatz/nanorc/archive/master.tar.gz' "${HOME}/.nano" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    # Install Ultimate Vim Configuration
-    PACKAGE_NAME='Ultimate vimrc'
-    printf -- "%sInstalling/updating %s...%s\n" "$BLUE" "$PACKAGE_NAME" "$RESET"
-    import_repo 'https://github.com/amix/vimrc/archive/master.tar.gz' "${HOME}/.vim_runtime" || {
-        error "import of ${PACKAGE_NAME} failed"
-        exit 1
-    }
-
-    # Install micro plugins
-    if command -v micro; then
-        printf -- "%sInstalling/updating micro plugins...%s\n" "$BLUE" "$RESET"
-        OUT_OF_DATE='installed but out-of-date'
-        if  micro -plugin install filemanager | tee -a /dev/tty | grep -q "$OUT_OF_DATE"; then
-            micro -plugin update  filemanager
-        fi
-        if  micro -plugin install manipulator | tee -a /dev/tty | grep -q "$OUT_OF_DATE"; then
-            micro -plugin update  manipulator
-        fi
-        if  micro -plugin install misspell | tee -a /dev/tty | grep -q "$OUT_OF_DATE"; then
-            micro -plugin update  misspell
-        fi
-        if  micro -plugin install misspell | tee -a /dev/tty | grep -q "$OUT_OF_DATE"; then
-            micro -plugin update  misspell
-        fi
-        if  micro -plugin install wc | tee -a /dev/tty | grep -q "$OUT_OF_DATE"; then
-            micro -plugin update  wc
-        fi
-    fi
 }
 
 # shellcheck source=/dev/null
@@ -183,7 +77,7 @@ setup_devtools() {
 
     # Install ASDF Versionn Manager
     # https://asdf-vm.com/
-    if ! command -v brew > /dev/null;; then
+    if ! command -v brew > /dev/null; then
         printf -- "%sInstalling/updating ASDF Extendable Version Manager...%s\n" "$BLUE" "$RESET"
         export ASDF_DIR="${ASDF_DIR:-$HOME/.asdf}" && (
             ASDF_NEW=false
