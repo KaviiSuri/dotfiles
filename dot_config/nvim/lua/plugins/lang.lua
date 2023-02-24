9738d33ceba4aa4d0503c5fd2ac6f737c190eb53
KaviiSuri <surikavii@gmail.com>
2023-02-24 14:57:42 +0530

    setup tailwind

--- dot_config/nvim/lua/plugins/lang.lua
+++ dot_config/nvim/lua/plugins/lang.lua
@@ -1,4 +1,5 @@
 return {
+  -- RUST
   "simrat39/rust-tools.nvim",
   {
     "saecki/crates.nvim",
@@ -10,6 +11,34 @@ return {
           name = "crates.nvim",
         },
       })
+    end,
+  },
+  -- Tailwind
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      opts.servers.tailwindcss = {}
+    end,
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+    },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      local format_kinds = opts.formatting.format
+      opts.formatting.format = function(entry, item)
+        format_kinds(entry, item)
+        return require("tailwindcss-colorizer-cmp").formatter(entry, item)
+      end
     end,
   },
 }
